42c6c6a55f8a0b38efcb1d8231f0b81c49aba6ab
Tuhin Nair <tuhinnair444@gmail.com>
2021-04-08 03:20:30 +0530

    optional quiet commands in behave:

--- features/steps/hello.py
+++ features/steps/hello.py
@@ -2,19 +2,26 @@ from behave import *
 import subprocess
 
 
-def run_cmd(cmd, capture_output=False):
+def run_cmd(cmd, capture_output=False, quiet=False):
+    if quiet:
+        return subprocess.run(
+            cmd.split(" "),
+            capture_output=capture_output,
+            stderr=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+        )
     return subprocess.run(cmd.split(" "), capture_output=capture_output)
 
 
 @given("we have ignition installed")
 def step_impl(context):
-    process = run_cmd("poetry run ignition -h")
+    process = run_cmd("poetry run ignition -h", quiet=True)
     assert process.returncode == 0
 
 
 @when("hello is passed in as a command")
 def step_impl(context):
-    process = run_cmd("poetry run ignition hello", True)
+    process = run_cmd("poetry run ignition hello", capture_output=True)
     context.response = process.stdout
 
 
@@ -25,7 +32,7 @@ def step_impl(context):
 
 @when("the hello command is passed in with a name option")
 def step_impl(context):
-    process = run_cmd("poetry run ignition hello -n tuhin", True)
+    process = run_cmd("poetry run ignition hello -n tuhin", capture_output=True)
     context.response = process.stdout
 
 
