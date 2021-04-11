a43c993e81c72c971c3b09dfa82ce594f3511e18
Tuhin Nair <tuhinnair444@gmail.com>
2021-04-11 06:55:34 +0530

    bump version

--- tests/test_ignition.py
+++ tests/test_ignition.py
@@ -5,7 +5,7 @@ import unittest
 
 class TestAppMeta(unittest.TestCase):
     def test_version(self):
-        self.assertEqual(__version__, "0.1.0")
+        self.assertEqual(__version__, "0.2.0")
 
 
 class TestAPI(unittest.TestCase):
